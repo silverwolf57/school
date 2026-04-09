3286e1f573e1479c8a6cd4fc56716aa8b152ba66
silverwolf57 <Hilbert.Gauss@outlook.com>
2026-04-09 16:56:12 +0800

    add the problem 简单的foreach

--- 程序设计实习/图像翻转/rotatebmp.cpp
+++ 程序设计实习/图像翻转/rotatebmp.cpp
@@ -37,31 +37,25 @@ struct RGBTRIPLE {
 
 int main(int argc, char* argv[]) {
     if (argc != 3) {
-        cout << "Usage: rotatebmp <src.bmp> <dest.bmp>" << endl;
+        cout << "Please enter the right command" << endl;
         return 1;
     }
-
     ifstream is(argv[1], ios::binary);
     if (!is) return 1;
-
     BITMAPFILEHEADER bfh;
     BITMAPINFOHEADER bih;
     is.read((char*)&bfh, sizeof(bfh));
     is.read((char*)&bih, sizeof(bih));
-
     int oldW = bih.biWidth;
     int oldH = bih.biHeight;
-
-    // 计算原图和新图的每行实际占用字节数（含Padding） [cite: 41, 94]
+    // 计算原图和新图的每行实际占用字节数
     int srcLineByte = ((oldW * 24 + 31) / 32) * 4;
     int destLineByte = ((oldH * 24 + 31) / 32) * 4;
-
     // 读取原图数据
     vector<unsigned char> srcData(srcLineByte * oldH);
     is.seekg(bfh.bfOffBits, ios::beg);
     is.read((char*)srcData.data(), srcData.size());
     is.close();
-
     // 更新新图的头信息
     int newW = oldH;
     int newH = oldW;
@@ -69,34 +63,23 @@ int main(int argc, char* argv[]) {
     bih.biHeight = newH;
     bih.biSizeImage = destLineByte * newH;
     bfh.bfSize = bfh.bfOffBits + bih.biSizeImage;
-
     vector<unsigned char> destData(bih.biSizeImage, 0);
-
-    // 旋转逻辑
-    // BMP在内存中是倒置存储的（最后一列像素其实是图像的第一行） 
+    // 旋转 
     for (int y = 0; y < oldH; y++) {
         for (int x = 0; x < oldW; x++) {
-            // 找到原图像素位置
             unsigned char* sP = &srcData[y * srcLineByte + x * 3];
-
-            // 顺时针旋转90度逻辑：
-            // 新坐标 (nx, ny) 其中 nx = y, ny = (newH - 1) - x
             int nx = y;
             int ny = (oldW - 1) - x;
-
             unsigned char* dP = &destData[ny * destLineByte + nx * 3];
-
-            dP[0] = sP[0]; // B
-            dP[1] = sP[1]; // G
-            dP[2] = sP[2]; // R
+            dP[0] = sP[0]; 
+            dP[1] = sP[1]; 
+            dP[2] = sP[2];
         }
     }
-
     ofstream os(argv[2], ios::binary);
     os.write((char*)&bfh, sizeof(bfh));
     os.write((char*)&bih, sizeof(bih));
     os.write((char*)destData.data(), destData.size());
     os.close();
-
     return 0;
 }
