e37fada5bf54da708a3c0750023434fdebac7097
silverwolf57 <Hilbert.Gauss@outlook.com>
2026-04-16 20:29:44 +0800

    solve 我自己的ostream_iterator.cpp

---
+++ 程序设计实习/STL1作业/我自己的ostream_iterator.cpp
@@ -0,0 +1,55 @@
+/*
+输入
+无
+输出
+5,21,14,2,3,
+1.4--5.56--3.2--98.3--3.3--
+样例输入
+无
+样例输出
+同输入
+*/
+#include <iostream>
+#include <list>
+#include <string>
+using namespace std;
+
+template <class T1,class T2>
+void Copy(T1 s,T1 e, T2 x)
+{
+	for(; s != e; ++s,++x)
+		*x = *s;
+}
+
+ 
+template<class T>
+class myostream_iteraotr
+{
+// 在此处补充你的代码
+private:
+ostream &fun;
+string div;
+public:
+myostream_iteraotr(ostream& f,string c):fun(f),div(c){}
+myostream_iteraotr operator=(T s){return *this;}
+myostream_iteraotr operator++(){return *this;}
+myostream_iteraotr operator*(T s){
+    fun<<s<<div;
+    return *this;
+} 
+         
+};
+
+
+int main()
+{	const int SIZE = 5;
+	int a[SIZE] = {5,21,14,2,3};
+	double b[SIZE] = { 1.4, 5.56,3.2,98.3,3.3};
+	list<int> lst(a,a+SIZE);
+	myostream_iteraotr<int> output(cout,",");
+	Copy( lst.begin(),lst.end(),output); 
+	cout << endl;
+	myostream_iteraotr<double> output2(cout,"--");
+	Copy(b,b+SIZE,output2);
+	return 0;
+}
